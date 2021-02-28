e8ea9417b5816880a2f8925843e321c79395a41e
Manu Mannattil <manu.mannattil@posteo.net>
2021-02-28 10:41:11 -0500

    rm OfflineIMAP: I use mbsync now.

--- offlineimap/.offlineimap.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from subprocess import check_output
-
-def get_pass(account):
-    """Retrieve password from password store using pass(1)."""
-    return check_output(["pass", account]).splitlines()[0]
