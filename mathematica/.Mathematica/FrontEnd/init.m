ab0c68792aba8f4af82bf939856b2a932983d174
Manu Mannattil <manu.mannattil@posteo.net>
2021-05-26 04:04:42 -0400

    mathematica: update to 12.3.0

--- mathematica/.Mathematica/FrontEnd/init.m
+++ mathematica/.Mathematica/FrontEnd/init.m
@@ -1,11 +1,14 @@
 SetOptions[$FrontEnd,
-VersionsLaunched->{"12.1.0"},
+VersionsLaunched->{"12.3.0"},
 PrivateFrontEndOptions->{"DialogSettings"->{
   "Login"->{"RememberMe"->True},
   "WelcomeScreen"->{"ShowRecentFilesContextMenu"->False}},
 "LastRegistrationReminderDate"->None,
 "ShowAtStartup"->"NewDocument",
-"WolframAlphaSettings"->{"Autoload"->False, "SendMathematicaSessionInfo" -> False}},
+"WolframAlphaSettings"->{"Autoload"->False, "SendMathematicaSessionInfo"->False}},
 PrivateNotebookOptions->{"FinalWindowPrompt"->False},
+(* Make variables local to current notebook. *)
+CellContext->Notebook,
+(* Disable predictive interface. *)
 ShowPredictiveInterface->False
 ]
